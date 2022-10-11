dcd0acf7e2f7644b73d05c28b6c226580ceb374d
HaDi Agah <38810979+HazNet@users.noreply.github.com>
2022-10-11 23:40:56 +0330

    turn on the first light of commit

--- Auth/AuthController.php
+++ Auth/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login', 'sendCode', 'register', 'checkCode', 'registerComplete', 'resetPassword']]);
+        #$this->middleware('auth:api', ['except' => ['login', 'sendCode', 'register', 'checkCode', 'registerComplete', 'resetPassword']]);
     }
 
     public function checkBeforeAuth($request)
